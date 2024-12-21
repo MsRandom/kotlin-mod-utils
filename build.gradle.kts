57adaccc0d5e77258ccb6d33ba053aa7a579465f
Ashley Wright <ashley@msrandom.net>
2024-12-22 02:57:42 +0300

    Add all targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-@file:OptIn(InternalKotlinGradlePluginApi::class)
-
-import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "2.0.20"
     id("earth.terrarium.cloche") version "0.5.0"
@@ -28,16 +23,72 @@ cloche {
     }
 
     forge("forge:1.16.5") {
-        loaderVersion.set("36.2.39")
+        loaderVersion.set("36.2.34")
         minecraftVersion.set("1.16.5")
 
         dependsOn(registryAccessMixin)
     }
 
+    forge("forge:1.18.2") {
+        loaderVersion.set("40.2.0")
+        minecraftVersion.set("1.18.2")
+    }
+
+    forge("forge:1.20.1") {
+        loaderVersion.set("47.1.3")
+        minecraftVersion.set("1.20.1")
+    }
+
+    neoforge("neoforge:1.21.1") {
+        loaderVersion.set("21.1.90")
+        minecraftVersion.set("1.21.1")
+    }
+
     fabric("fabric:1.16.5") {
-        loaderVersion.set("0.14.8")
+        loaderVersion.set("0.16.9")
         minecraftVersion.set("1.16.5")
 
+        noClient()
+
+        dependencies {
+            fabricApi("0.42.0+1.16")
+        }
+
         dependsOn(registryAccessMixin)
+    }
+
+    fabric("fabric:1.18.2") {
+        loaderVersion.set("0.16.9")
+        minecraftVersion.set("1.18.2")
+
+        noClient()
+
+        dependencies {
+            fabricApi("0.77.0+1.18.2")
+        }
+
+        dependsOn(registryAccessMixin)
+    }
+
+    fabric("fabric:1.20.1") {
+        loaderVersion.set("0.16.9")
+        minecraftVersion.set("1.20.1")
+
+        noClient()
+
+        dependencies {
+            fabricApi("0.92.2+1.20.1")
+        }
+    }
+
+    fabric("fabric:1.21.1") {
+        loaderVersion.set("0.16.9")
+        minecraftVersion.set("1.21.1")
+
+        noClient()
+
+        dependencies {
+            fabricApi("0.110.0+1.21.1")
+        }
     }
 }
